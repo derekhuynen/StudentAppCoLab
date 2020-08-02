c0c8d8e10f0c45bf855af747d055eda24978fe2d
Derek <derek.huynen@gmail.com>
2020-08-01 19:22:51 -0700

    added parent class

--- src/main/java/com/delsolschool/studentcolab/model/Person.java
+++ src/main/java/com/delsolschool/studentcolab/model/Person.java
@@ -1,14 +1,11 @@
 package com.delsolschool.studentcolab.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.sql.Date;
 
+
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS )
 @Table(name = "person")
 public class Person {
 
