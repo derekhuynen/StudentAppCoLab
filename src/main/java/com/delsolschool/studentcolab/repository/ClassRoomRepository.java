10c1ec764efb4fca8b47a37242c6b38f4b9d6453
Derek <derek.huynen@gmail.com>
2020-08-01 19:32:47 -0700

    Revert "Added Classroom MVC"
    This reverts commit cde16bcf3aadd8e3f040ce6ae84aaba8209c96af.

--- src/main/java/com/delsolschool/studentcolab/repository/ClassRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.delsolschool.studentcolab.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.delsolschool.studentcolab.model.Classroom;
-
-public interface ClassRoomRepository extends CrudRepository<Classroom, Integer>{
-}
